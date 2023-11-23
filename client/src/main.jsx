a7ba06d4237685f97372648fd69858740bd5c1fd
jhyep <pjh1115_@naver.com>
2023-11-24 00:21:01 +0900

    chore: GlobalStyle 적용

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
 import './index.css';
-import router from './routes/router.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import router from './routes/router.jsx';
+import GlobalStyle from './styles/GlobalStyle';
 import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+  input {
+    border: none;
+    outline: none;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  p, h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+  }
+    
+`;
+
+export default GlobalStyle;
