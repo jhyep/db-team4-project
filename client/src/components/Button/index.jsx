9189337f13cf73ab98341b0b87bb4724e91359b6
jhyep <pjh1115_@naver.com>
2023-11-26 17:21:24 +0900

    feat: 버튼 컴포넌트 구현

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,5 +1,27 @@
-function Button({ children }) {
-  return <button>{children}</button>;
+import palette from '../../styles/palette';
+
+function Button({
+  children,
+  color,
+  width,
+  height,
+  backgroundColor,
+  onClick,
+  ...restProps
+}) {
+  const style = {
+    color: color || '#fff',
+    width: width || '80px',
+    height: height || '34px',
+    backgroundColor: backgroundColor || `${palette.mainYellow}`,
+    fontSize: '14px',
+  };
+
+  return (
+    <button style={style} onClick={onClick} {...restProps}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
