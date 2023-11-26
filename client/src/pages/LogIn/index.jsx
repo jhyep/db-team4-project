4f81f9aeccea21938e0a9a603339a46e47e01fec
sunchipq <wjsalswo369@naevr.com>
2023-11-27 04:45:49 +0900

    feat: axios 구현(login, signup)

--- client/src/pages/LogIn/index.jsx
+++ client/src/pages/LogIn/index.jsx
@@ -1,8 +1,26 @@
 import * as S from './style';
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 function LogIn() {
+  const [userid, setUserid] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5173/login', {
+        userid: userid,
+        password: password,
+      });
+      console.log('서버 응답: ', response.data);
+    } catch (err) {
+      console.log('로그인 오류', err);
+    }
+  }
+
   return (
     <>
       <S.Container>
@@ -10,10 +28,22 @@ function LogIn() {
           <h2>로그인</h2>
         </S.Title>
         <S.InputWrapper>
-          <S.InputBox type="text" placeholder="아이디" />
-          <S.InputBox type="password" placeholder="비밀번호" />
+          <S.InputBox
+            type="text"
+            placeholder="아이디"
+            value={userid}
+            onChange={(e) => setUserid(e.target.value)}
+          />
+          <S.InputBox
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </S.InputWrapper>
-        <Button width="100%">로그인</Button>
+        <Button type="submit" onClick={handleLogin} width="100%">
+          로그인
+        </Button>
         <S.SuggestionWrap>
           <p>
             아직 회원이 아니신가요?<Link to="/signup">회원가입</Link>
--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -2,8 +2,34 @@ import * as S from '../LogIn/style';
 import styled from 'styled-components';
 import Button from '../../components/Button';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 function SignUp() {
+  const [userid, setUserid] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpwd, setConfirmpwd] = useState('');
+  const [username, setUsername] = useState('');
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    if (password != confirmpwd) {
+      return alert('일치하지 않는 비밀번호');
+    }
+
+    try {
+      const response = await axios.post('http://localhost:5173/signup', {
+        userid: userid,
+        password: password,
+        username: username,
+      });
+      console.log('서버 응답: ', response.data);
+    } catch (err) {
+      console.log('로그인 오류', err);
+    }
+  }
+
   return (
     <>
       <S.Container>
@@ -11,12 +37,34 @@ function SignUp() {
           <h2>회원가입</h2>
         </S.Title>
         <S.InputWrapper>
-          <S.InputBox type="text" placeholder="이름" />
-          <S.InputBox type="text" placeholder="아이디" />
-          <S.InputBox type="password" placeholder="비밀번호" />
-          <S.InputBox type="password" placeholder="비밀번호 확인" />
+          <S.InputBox
+            type="text"
+            placeholder="이름"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <S.InputBox
+            type="text"
+            placeholder="아이디"
+            value={userid}
+            onChange={(e) => setUserid(e.target.value)}
+          />
+          <S.InputBox
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <S.InputBox
+            type="password"
+            placeholder="비밀번호 확인"
+            value={confirmpwd}
+            onChange={(e) => setConfirmpwd(e.target.value)}
+          />
         </S.InputWrapper>
-        <Button width="100%">회원가입</Button>
+        <Button type="submit" onClick={handleSignUp} width="100%">
+          회원가입
+        </Button>
         <GuideWrap>
           <p>계정이 이미 있는 경우에는</p>
           <p>
