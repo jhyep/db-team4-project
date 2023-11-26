52e3a7b6b64ed9706d07eb70e6eccdcd5a618fd5
jhyep <pjh1115_@naver.com>
2023-11-26 17:22:49 +0900

    feat: 라우터에 회원가입 + 로그인 페이지 추가

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,4 +1,6 @@
 import App from '../App';
+import LogIn from '../pages/LogIn';
+import SignUp from '../pages/SignUp';
 import { Outlet, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
@@ -11,8 +13,8 @@ const router = createBrowserRouter([
         element: <Outlet />,
         children: [
           { index: true, element: <div>main</div> },
-          { path: 'signup', element: <div>회원가입</div> },
-          { path: 'login', element: <div>로그인</div> },
+          { path: 'signup', element: <SignUp /> },
+          { path: 'login', element: <LogIn /> },
           { path: 'mypage', element: <div>마이페이지</div> },
           { path: 'search', element: <div>검색 결과 페이지</div> },
           {
