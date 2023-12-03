ff0921896eaaea11540e65216570f72a916a18ea
sunchipq <wjsalswo369@naevr.com>
2023-12-03 19:45:24 +0900

    fix: git conflict 수정

--- client/src/pages/Search/index.jsx
+++ client/src/pages/Search/index.jsx
@@ -1,26 +1,18 @@
 import { useState } from 'react';
-import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import * as S from './style';
+import SearchMenu from '../../components/SearchMenu';
+import PageContainer from '../../components/PageContainer';
+import ContentsBox from '../../components/ContentsBox';
 
 function Search() {
-  const [searchWord, setSearchWord] = useState('');
-  const [searchType, setSearchType] = useState('Keyword');
-  const [searchSort, setSearchSort] = useState('Accuracy');
-  const [searchRecentPublish, setSearchRecentPublish] = useState('0');
-  const [searchResult, setSearchResult] = useState([]);
+  const location = useLocation();
+  const { searchWord, searchResult } = location.state || {};
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
-  const onSearchWordChange = (e) => {
-    setSearchWord(e.target.value);
-  };
-  const onSearchTypeChange = (e) => {
-    setSearchType(e.target.value);
-  };
-  const onSearchSortChange = (e) => {
-    setSearchSort(e.target.value);
-    console.log(searchSort);
-  };
-  const onSearchRecentPublishChange = (e) => {
-    setSearchRecentPublish(e.target.value);
-  };
+  function handleMouseEnter(index) {
+    setHoveredIndex(index);
+  }
 
   async function saveBook(book) {
     if (!sessionStorage.getItem('userid')) {
@@ -40,125 +32,45 @@ function Search() {
       console.log('error ', err);
     }
   }
-
-  async function bookSearch(e) {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('/bookinfo/search', {
-        query: searchWord,
-        queryType: searchType,
-        sort: searchSort,
-        recentPublishFilter: searchRecentPublish,
-      });
-
-      setSearchResult(response.data);
-    } catch (err) {
-      console.log('failed to request', err);
-    }
+  function handleMouseLeave() {
+    setHoveredIndex(null);
   }
 
   return (
-    <>
-      <div>
-        <div>
-          검색어:{' '}
-          <input
-            type="search"
-            name="searchWord"
-            onChange={onSearchWordChange}
-            style={{ border: '1px solid' }}
-          />
-          <input
-            type="radio"
-            name="searchType"
-            value="Keyword"
-            onChange={onSearchTypeChange}
-            defaultChecked
-          />
-          제목 + 저자
-          <input
-            type="radio"
-            name="searchType"
-            value="Title"
-            onChange={onSearchTypeChange}
-          />
-          제목
-          <input
-            type="radio"
-            name="searchType"
-            value="Author"
-            onChange={onSearchTypeChange}
-          />
-          저자
-          <input
-            type="radio"
-            name="searchType"
-            value="Publisher"
-            onChange={onSearchTypeChange}
-          />
-          출판사
-        </div>
-        <div>
-          정렬순:{' '}
-          <select name="searchSort" onChange={onSearchSortChange}>
-            <option value="Accuracy">관련도</option>
-            <option value="PublishTime">출간일</option>
-            <option value="Title">제목</option>
-            <option value="SalesPoint">판매량</option>
-            <option value="CustomerRating">고객평점(알라딘)</option>
-            <option value="MyReviewCount">마이리뷰개수(알라딘)</option>
-          </select>
-        </div>
-        <div>
-          최근 n개월 내 출판:{' '}
-          <input
-            type="number"
-            name="searchRecentPublish"
-            min="0"
-            max="60"
-            step="1"
-            defaultValue={'0'}
-            onChange={onSearchRecentPublishChange}
-            style={{ border: '1px solid' }}
-          />{' '}
-          (0은 제한 없음)
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={bookSearch}
-            style={{ border: '1px solid' }}
+    <PageContainer>
+      <S.Container>
+        <ContentsBox width="1024px">
+          <SearchMenu />
+        </ContentsBox>
+      </S.Container>
+
+      <S.SearchWordSpan>&apos;{searchWord}&apos;</S.SearchWordSpan>
+      <span> 에 대한 검색 결과입니다.</span>
+      <S.SearchResultContainer>
+        {searchResult.map((result, index) => (
+          <S.InfoContainer
+            key={index}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
           >
-            검색
-          </button>
-        </div>
-        <div>
-          {searchResult.map((item, index) => {
-            return (
-              <div key={`search-result-${index}`} style={{ marginTop: '1em' }}>
-                <p>(____제목) {item.title}</p>
-                <p>(____저자) {item.author}</p>
-                <p>(__출판사) {item.publisher}</p>
-                <p>(__출간일) {item.pubDate}</p>
-                <p>
-                  (카테고리) {item.categoryName ? item.categoryName : '(없음)'}
-                </p>
-                <p>(__시리즈) {item.seriesName ? item.seriesName : '(없음)'}</p>
-                <button
-                  type="button"
-                  onClick={() => saveBook(item)}
-                  style={{ border: '1px solid' }}
-                >
-                  {' '}
-                  읽은책
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </>
+            <S.Cover
+              src={result.cover}
+              alt="커버"
+              width="150px"
+              height="220px"
+            />
+            <S.Title
+              style={{
+                textDecoration: hoveredIndex === index ? 'underline' : 'none',
+              }}
+            >
+              {result.title}
+            </S.Title>
+            <S.Author>{result.author}</S.Author>
+          </S.InfoContainer>
+        ))}
+      </S.SearchResultContainer>
+    </PageContainer>
   );
 }
 
