492ac2d16e043e48ba8ae7ebc3088d3408bba71e
afterst0p <kingwlgn@gmail.com>
2023-12-05 18:39:03 +0900

    feat: 책 조회시 DB에 데이터 추가

--- server/routes/book.js
+++ server/routes/book.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 
 const { searchBook } = require("./bookUtil/searchBook.js");
 const { infoBook } = require("./bookUtil/infoBook.js");
-const { getRate } = require("./dbUtil/dbUtils.js");
 const { deleteBook } = require("./bookUtil/deleteBook.js");
 const { getReads } = require("./bookUtil/getReads.js");
+const { dbGetRate } = require("./dbUtil/dbBookUtils/dbBookUtils.js");
+const { dbAddRate } = require("./dbUtil/dbBookUtils/dbBookUtils.js");
 
 router.post("/search", async function (req, res) {
   const requestData = req.body;
@@ -42,20 +43,37 @@ router.post("/info", async function (req, res) {
   }
 });
 
-router.post("/rate", async function (req, res) {
+router.post("/getrate", async function (req, res) {
   const requestData = req.body;
 
   if (requestData) {
     try {
-      const reviews = await getRate(requestData.itemId);
+      const reviews = await dbGetRate(requestData.itemId);
       res.send(reviews);
     } catch (err) {
       res.status(500).send("Internal Server Error");
-      console.error("Internal Server Error while get Reviews: ", err);
+      console.error("Internal Server Error while get ratings: ", err);
     }
   } else {
     res.status(400).send("Bad Request");
-    console.error("Bad request for search api: ", err);
+    console.error("Bad request for getrate api: ", err);
+  }
+});
+
+router.post("/addrate", async function (req, res) {
+  const requestData = req.body;
+
+  if (requestData) {
+    try {
+      const result = await dbAddRate(requestData);
+      res.send(result);
+    } catch (err) {
+      res.status(500).send("Internal Server Error");
+      console.error("Internal Server Error while add rate: ", err);
+    }
+  } else {
+    res.status(400).send("Bad Request");
+    console.error("Bad request for addrate api: ", err);
   }
 });
 
--- server/routes/dbUtil/dbBookUtils/dbBookUtils.js
+++ server/routes/dbUtil/dbBookUtils/dbBookUtils.js
@@ -8,6 +8,136 @@ const connectionConfig = {
   connectString: dbConfig.connectString,
 };
 
+async function dbBookInsert(bkInfo) {
+  let connection, sql, binds, result;
+
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+
+    // db에 책이 있는지 확인
+    sql = "select count(*) from book where isbn13=:isbn13";
+    binds = {
+      isbn13: bkInfo.isbn13,
+    };
+    result = await connection.execute(sql, binds);
+
+    // 없으면 책 추가
+    if (result.rows[0][0] == 0) {
+      sql =
+        "insert into book values(:title, :author, :isbn13, TO_DATE(:pubDate, 'YYYY-MM-DD'), :publisher, 0, :series_id, 0)";
+      binds = {
+        title: bkInfo.title.substring(0, 83),
+        author: bkInfo.author.substring(0, 20),
+        isbn13: bkInfo.isbn13,
+        pubDate: bkInfo.pubDate,
+        publisher: bkInfo.publisher.substring(0, 20),
+        series_id: bkInfo.seriesId,
+      };
+      await connection.execute(sql, binds);
+      await connection.execute("COMMIT");
+
+      // 작가 있는지 확인
+      sql = "select count(*) from author where author_name=:author_name";
+      binds = {
+        author_name: bkInfo.author.substring(0, 20),
+      };
+      result = await connection.execute(sql, binds);
+
+      // 없으면 작가 추가
+      if (result.rows[0][0] == 0) {
+        sql =
+          "select author_id from author where rownum <= 1 order by author_id desc";
+        result = await connection.execute(sql);
+
+        sql = "insert into author values(:author_id, :author_name)";
+        binds = {
+          author_id: result.rows[0][0] + 1,
+          author_name: bkInfo.author.substring(0, 20),
+        };
+        result = await connection.execute(sql, binds);
+        await connection.execute("COMMIT");
+      }
+
+      // 작가와 책 연결
+      sql = "select author_id from author where author_name=:author_name";
+      binds = {
+        author_name: bkInfo.author.substring(0, 20),
+      };
+      result = await connection.execute(sql, binds);
+
+      sql = "insert into writes values(:author_id, :isbn13)";
+      binds = {
+        author_id: result.rows[0][0],
+        isbn13: bkInfo.isbn13,
+      };
+      await connection.execute(sql, binds);
+      await connection.execute("COMMIT");
+
+      if (bkInfo.categoryId) {
+        // 카테고리 있는지 확인
+        sql = "select count(*) from category where category_id=:category_id";
+        binds = {
+          category_id: bkInfo.categoryId,
+        };
+        result = await connection.execute(sql, binds);
+
+        // 없으면 카테고리 추가
+        if (result.rows[0][0] == 0) {
+          sql = "insert into category values(:category_id, :category_name)";
+          binds = {
+            category_id: bkInfo.categoryId,
+            category_name: bkInfo.categoryName.substring(0, 50),
+          };
+          result = await connection.execute(sql, binds);
+          await connection.execute("COMMIT");
+        }
+      }
+
+      // 카테고리와 책 연결
+      sql = "insert into has values(:category_id, :isbn13)";
+      binds = {
+        category_id: bkInfo.categoryId,
+        isbn13: bkInfo.isbn13,
+      };
+      await connection.execute(sql, binds);
+      await connection.execute("COMMIT");
+
+      if (bkInfo.seriesId) {
+        // 시리즈 있는지 확인
+        sql = "select count(*) from series where series_id=:series_id";
+        binds = {
+          series_id: bkInfo.seriesId,
+        };
+        result = await connection.execute(sql, binds);
+
+        // 없으면 시리즈 추가
+        if (result.rows[0][0] == 0) {
+          sql = "insert into series values(:series_id, :series_name)";
+          binds = {
+            series_id: bkInfo.seriesId,
+            series_name: bkInfo.seriesName.substring(0, 33),
+          };
+          result = await connection.execute(sql, binds);
+          await connection.execute("COMMIT");
+        }
+      }
+    }
+
+    return true;
+  } catch (err) {
+    console.error("Insert Error: ", err);
+    return null;
+  } finally {
+    if (connection) {
+      try {
+        connection.close();
+      } catch (err) {
+        console.error("Connection Close Error: ", err);
+      }
+    }
+  }
+}
+
 async function dbBookDelete(isbn13) {
   let connection, sql, binds, result;
   try {
@@ -57,8 +187,74 @@ async function dbGetReads(userid) {
   }
 }
 
+async function dbGetRate(isbn13) {
+  let connection, sql, binds, result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+
+    sql = "select * from rating where isbn13 = :isbn13";
+    binds = { isbn13 };
+    result = await connection.execute(sql, binds);
+
+    const rateSet = [];
+
+    for (const rate of result.rows) {
+      rateSet.push({
+        userId: rate[3],
+        rating: rate[1],
+        comment: rate[2],
+      });
+    }
+
+    return rateSet;
+  } catch (err) {
+    console.error("getReview Error: ", err);
+    return null;
+  } finally {
+    if (connection) {
+      try {
+        connection.close();
+      } catch (err) {
+        console.error("Connection Close Error: ", err);
+      }
+    }
+  }
+}
+
+async function dbAddRate(ratingTuple) {
+  let connection, sql, binds, result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+
+    sql = "insert into rating values(0, :rating, :contents, :user_id, :isbn13)";
+    binds = {
+      rating: ratingTuple.rating,
+      contents: ratingTuple.contents,
+      user_id: ratingTuple.user_id,
+      isbn13: ratingTuple.isbn13,
+    };
+    result = await connection.execute(sql, binds);
+    await connection.execute("COMMIT");
+
+    return result;
+  } catch (err) {
+    console.error("addReview Error: ", err);
+    return null;
+  } finally {
+    if (connection) {
+      try {
+        connection.close();
+      } catch (err) {
+        console.error("Connection Close Error: ", err);
+      }
+    }
+  }
+}
 
 module.exports = {
   dbBookDelete,
+  dbBookInsert,
   dbGetReads,
+  dbGetRate,
+  dbAddRate,
 };
