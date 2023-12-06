a616b4dca041d324b9095e507aacf0558d46400b
jhyep <pjh1115_@naver.com>
2023-12-06 16:38:59 +0900

    fix: dbConfig 수정

--- server/routes/dbUtil/dbConfig.js
+++ server/routes/dbUtil/dbConfig.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   user: process.env.NODE_ORALCEDB_USER || "book",
-  password: process.env.NODE_ORALCEDB_PASSWORD || "book1",
+  password: process.env.NODE_ORALCEDB_PASSWORD || "comp322",
   connectString:
     process.env.NODE_ORALCEDB_CONNECTIONSTRING || "localhost:1521/orcl",
   externalAuth: process.env.NODE_ORALCEDB_EXTERNALAUTH ? true : false,
